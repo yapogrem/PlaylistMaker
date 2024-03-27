e2a79425d8e1f0f2d1f12c9cdfc7da3ce8c54042
pogre <0961@list.ru>
2024-03-27 11:35:23 +0300

    Sp 12 v1.0

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -1,14 +1,19 @@
 package com.example.playlistmaker
 import android.view.ViewGroup
+
 import androidx.recyclerview.widget.RecyclerView
 
 
-class TrackAdapter() : RecyclerView.Adapter<TrackViewHolder> () {
+class TrackAdapter(val searchHistory: SearchHistory) : RecyclerView.Adapter<TrackViewHolder> () {
     var tracks = ArrayList<Track>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         return TrackViewHolder(parent)
     }
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        val track = tracks[position]
+        holder.itemView.setOnClickListener {
+            searchHistory.saveTrackInSearchHistory(track)
+        }
         holder.bind(tracks[position])
     }
     override fun getItemCount():Int = tracks.size
